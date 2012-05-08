0edb6721c0cf41541a101329ffe02570d80eb384
bonidjukic <bonidjukic@gmail.com>
2012-05-08 23:42:09 +0200

    Eggdb - badges

---
+++ db_schema/eggdb.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE IF NOT EXISTS eggdb COLLATE = 'utf8_unicode_ci';
+
+CREATE TABLE  `eggdb`.`badges` (
+    `badge_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    `link` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
+    `name` VARCHAR( 32 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
+    `description` VARCHAR( 1000 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL ,
+    `parent` INT UNSIGNED NOT NULL ,
+    `type` ENUM(  'category',  'boolean' ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT  'boolean',
+    INDEX `INDEX` (  `parent` ,  `type` )
+) ENGINE = INNODB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE  `eggdb`.`users` (
+    `user_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    `first_name` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL ,
+    `last_name` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL ,
+    `email` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE = INNODB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE  `eggdb`.`badges_users` (
+    `badge_id` BIGINT UNSIGNED NOT NULL ,
+    `user_id` BIGINT UNSIGNED NOT NULL ,
+    `value` TINYINT( 1 ) NOT NULL ,
+    `description` VARCHAR( 1000 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL ,
+    PRIMARY KEY (  `badge_id` ,  `user_id` ) ,
+    INDEX `INDEX` (  `badge_id` ,  `user_id` ,  `value` )
+) ENGINE = INNODB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
