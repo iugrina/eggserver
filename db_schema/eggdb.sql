5d33e02bfce1419d778711f1bbb050977b9c625d
bonidjukic <bonidjukic@gmail.com>
2012-05-21 10:53:08 +0200

    Badge type column changed from ENUM to TINYINT

--- db_schema/eggdb.sql
+++ db_schema/eggdb.sql
@@ -8,16 +8,17 @@
 -- Table structure for table `badges`
 --
 
-CREATE TABLE  `eggdb`.`badges` (
-    `badge_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    `link` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
-    `name` VARCHAR( 32 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
-    `description` VARCHAR( 1000 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL ,
-    `parent` INT UNSIGNED NOT NULL ,
-    `type` ENUM(  'category',  'boolean' ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT  'boolean' ,
-    INDEX `parent` (  `parent` ) ,
-    INDEX `type` (  `type` )
-) ENGINE = INNODB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE TABLE IF NOT EXISTS `badges` (
+  `badge_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `link` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(1000) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `parent` int(10) unsigned NOT NULL,
+  `type` tinyint(4) unsigned NOT NULL,
+  PRIMARY KEY (`badge_id`),
+  KEY `parent` (`parent`),
+  KEY `type` (`type`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=40 ;
 
 -- --------------------------------------------------------
 
@@ -25,12 +26,16 @@ CREATE TABLE  `eggdb`.`badges` (
 -- Table structure for table `badges_users`
 --
 
-CREATE TABLE  `eggdb`.`users` (
-    `user_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-    `first_name` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL ,
-    `last_name` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL ,
-    `email` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
-) ENGINE = INNODB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE TABLE IF NOT EXISTS `badges_users` (
+  `badge_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `value` tinyint(1) NOT NULL,
+  `description` varchar(1000) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`badge_id`,`user_id`),
+  KEY `badge_id` (`badge_id`),
+  KEY `user_id` (`user_id`),
+  KEY `value` (`value`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -38,20 +43,21 @@ CREATE TABLE  `eggdb`.`users` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE  `eggdb`.`badges_users` (
-    `badge_id` BIGINT UNSIGNED NOT NULL ,
-    `user_id` BIGINT UNSIGNED NOT NULL ,
-    `value` TINYINT( 1 ) NOT NULL ,
-    `description` VARCHAR( 1000 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL ,
-    PRIMARY KEY (  `badge_id` ,  `user_id` ) ,
-    INDEX `badge_id` (  `badge_id`  ) ,
-    INDEX `user_id` (  `user_id`  ) ,
-    INDEX `value` (  `value`  )
-) ENGINE = INNODB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `last_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+
+--
+-- Constraints for dumped tables
+--
 
 --
 -- Constraints for table `badges_users`
 --
 ALTER TABLE `badges_users`
-  ADD CONSTRAINT `badges_users_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `badges_users_ibfk_1` FOREIGN KEY (`badge_id`) REFERENCES `badges` (`badge_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `badges_users_ibfk_1` FOREIGN KEY (`badge_id`) REFERENCES `badges` (`badge_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `badges_users_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
