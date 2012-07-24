d34ff23d31a872d1f9d9d4c3ee91715bf4852c8c
Nikola Henezi <nikola.henezi@gmail.com>
2012-07-24 13:13:33 +0200

    sqlAlchemy addded, events name fixed, settings and basic app added

---
+++ app/app.py
@@ -0,0 +1,45 @@
+import tornado.ioloop
+import tornado.web
+from sqlalchemy import select
+
+from tornado.options import options
+import settings
+import sqlalchemy
+
+class Application(tornado.web.Application):
+  def __init__(self):
+    handlers = [
+      (r"/events/", EventsHandler),
+    ]
+
+    settings = dict(
+      debug=True,
+    )
+
+    self.db = sqlalchemy.create_engine(options.engine + "://" + options.user + ":" +
+                                       options.password + "@" + options.host + "/" +
+                                       options.database)
+    self.metadata  = sqlalchemy.MetaData(bind=self.db)
+    self.db.echo = "debug"
+    tornado.web.Application.__init__(self, handlers, **settings)
+
+
+class BaseHandler(tornado.web.RequestHandler):
+  @property
+  def db(self):
+    return self.application.db
+
+  @property
+  def metadata(self):
+    return self.application.metadata
+
+
+class EventsHandler(BaseHandler):
+  def get(self):
+    users = sqlalchemy.Table("users", self.metadata)
+    users.select().execute().first()
+
+if __name__ == "__main__":
+  app = Application()
+  app.listen(options.port)
+  tornado.ioloop.IOLoop.instance().start()
