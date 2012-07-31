bc7b7f24aa9cbabf5a27da35f04c0a680fee91ed
bonidjukic <bonidjukic@gmail.com>
2012-07-31 21:44:41 +0200

    Badges tree generating

--- tools/badges_frontend/static/js/badges.js
+++ tools/badges_frontend/static/js/badges.js
@@ -4,10 +4,12 @@ $(function() {
     $("#add-new-form").toggle('slow');
     return false;
   });
+  
   $("#add-new-cancel").click(function() {
     $("#add-new-form").hide('slow');
     return false;
   });
+  
   $(".table tr").hover(
     function () {
       $(this).find(".btn-danger, .btn-inverse").toggle();
@@ -16,12 +18,14 @@ $(function() {
       $(this).find(".btn-danger, .btn-inverse").toggle();
     }
   );
+  
   $(".btn-delete-badge").click(function() {
     bid = "bid-" + $(this).attr("id").slice(4);
     $("#delete-badge-modal .btn-delete-badge-confirm").attr("id", bid);
     $('#delete-badge-modal').modal('show');
     return false;
   });
+  
   $(".btn-delete-badge-confirm").click(function() {
     bid = $(this).attr("id").slice(4);
     $.get('/badges/delete/' + bid, function(data) {
@@ -30,6 +34,7 @@ $(function() {
     $('#delete-badge-modal').modal('hide');
     return false;
   });
+  
   $(".home-link").click(function() {
     $("#badges-list").show();
     $(".home-link").addClass("active");
@@ -38,12 +43,47 @@ $(function() {
     $("#add-new-form").hide('slow');
     return false;
   });
+  
   $(".tree-link").click(function() {
     $("#badges-list").hide();
     $(".home-link").removeClass("active");
     $("#badges-tree").show();
     $(".tree-link").addClass("active");
     $("#add-new-form").hide('slow');
+  
+    // Render tree
+    $.get('tree/get', function(data) {
+      renderTree(data);
+    });
+    
     return false;
   });
 });
+
+function renderTree(data) {
+  var iter = 0;
+  var badges = $.parseJSON(data);
+  var tbody = $("#badges-tree tbody");
+  
+  for (x in badges) {
+    createRow(badges[x], tbody, iter);
+  }
+}
+
+function createRow(badge, tbody, iter) {
+  var padding = 'padding-left: 15px;';
+  var childImg = '';
+  if (badge.node.parent != 0) {
+    padding = 'padding-left: ' + (10 + iter * 20) + 'px;';
+    childImg = '<img src="static/img/tree.gif" class="hierarchy-tree-line">';
+  }
+  
+  iter++;
+  
+  tbody.append('<tr><td style="' + padding + '">' + childImg + badge.node.name + '</td></tr>');
+  if (badge.children) {
+    for (x in badge.children) {
+      createRow(badge.children[x], tbody, iter);
+    }
+  }
+}
