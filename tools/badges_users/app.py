005c5df6055f15c23a5b9cc24400a30c9cd1d9b0
bonidjukic <bonidjukic@gmail.com>
2012-08-10 22:06:49 +0200

    Renamed handlers so we can add new handlers for users

--- tools/badges_users/app.py
+++ tools/badges_users/app.py
@@ -16,7 +16,7 @@ class MainHandler(ProtoHandler):
     badges = self.db.query("select * from badges")
     self.render("badges.html", badges=badges, added_new=False, error=False)
     
-class AddNewHandler(ProtoHandler):
+class AddNewBadgeHandler(ProtoHandler):
   def post(self):
     try:
       name = self.get_argument("name")
@@ -47,13 +47,13 @@ class AddNewHandler(ProtoHandler):
       badges = self.db.query("select * from badges")
       self.render("badges.html", badges=badges, added_new=False, error=True)
       
-class GetHandler(ProtoHandler):
+class GetBadgeHandler(ProtoHandler):
   def get(self, bid):
     result = self.db.query("select * from badges where badge_id=%s", bid)
       
     self.write(json.dumps(result))
       
-class DeleteHandler(ProtoHandler):
+class DeleteBadgeHandler1(ProtoHandler):
   def get(self, bid):
     try:
       result = self.db.query("select link from badges where badge_id=%s", bid)
@@ -102,9 +102,9 @@ settings = {
 application = tornado.web.Application([
   (r"/", MainHandler, dict(db=db)),
   (r"/tree/get", GenerateTree, dict(db=db)),
-  (r"/badges/add", AddNewHandler, dict(db=db)),
-  (r"/badges/get/([^/]+)", GetHandler, dict(db=db)),
-  (r"/badges/delete/([^/]+)", DeleteHandler, dict(db=db)),
+  (r"/badges/add", AddNewBadgeHandler, dict(db=db)),
+  (r"/badges/get/([^/]+)", GetBadgeHandler, dict(db=db)),
+  (r"/badges/delete/([^/]+)", DeleteBadgeHandler, dict(db=db)),
 ], **settings)
 
 if __name__ == "__main__":
