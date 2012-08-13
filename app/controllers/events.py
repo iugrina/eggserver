e9b9d0ee3772b7a930b7fa5094bc24fca19398e6
Nikola Henezi <nikola.henezi@gmail.com>
2012-08-13 12:42:29 +0200

    Event base class added
    * All event classes extend frmo eventBase
    * eventBase handles databases and models.
    
    refs #37

--- app/controllers/events.py
+++ app/controllers/events.py
@@ -3,11 +3,12 @@ from models.events.event import Event
 from common import utils
 from pprint import pprint as pp
 
-class EventsHandler(tornado.web.RequestHandler):
+class EventBase(tornado.web.RequestHandler):
   def initialize(self, db):
     self.db = db
     self.event = Event(self.db)
 
+class EventsHandler(EventBase):
   def get(self):
     event_id = self.get_argument("event_id")
     result = self.event.get_event(event_id)
@@ -19,6 +20,9 @@ class EventsHandler(tornado.web.RequestHandler):
     for param in self.request.arguments:
       params[param] = self.request.arguments[param][0]
     self.write(utils.json.dumps(params))
+
+class EventHandler(EventBase):
+  pass
 
     #CURL test
     #curl -F "name=testiram" -F "description=ovo je description" -F "scheduled_for=2012-09-23T00:00:00" -F "expected_duration=17:00:00" -F "registration_deadline=2012-09-18T00:00:00" -F "location=Zagreb"  -F "hide_location=1" -F "registration_price=300" -F "creation_price=200" -F "is_active=1" -F "phase=before_event" localhost:8888/events/
