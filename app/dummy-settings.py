ccb50b108cf6f1b1a0308778df16b9618d007d9f
Nikola Henezi <nikola.henezi@gmail.com>
2012-08-16 12:21:42 +0200

    removed old settings

--- app/dummy-settings.py
+++
@@ -1,7 +0,0 @@
-from tornado.options import define
-define("engine", default="mysql", help="database engine (mysql, sqlite..)")
-define("port", default=8888, help="run on the given port", type=int)
-define("host", default="localhost", help="database host")
-define("database", default="egg", help="database name")
-define("user", default="user", help="database user")
-define("password", default="password", help="database password")
