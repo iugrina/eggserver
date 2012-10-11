18ceb255d5a774a87076d8d1e5f2528af0e7de52
Ivo Ugrina <ivo@iugrina.com>
2012-10-11 20:21:54 +0200

    added create-friends_table db migration

---
+++ db_schema/0006.create-friends_table.py
@@ -0,0 +1,13 @@
+step(
+  """
+  CREATE TABLE `friends` (
+    `user_id` bigint(20) unsigned NOT NULL,
+    `friend_id` bigint(20) unsigned NOT NULL,
+    PRIMARY KEY (`user_id`,`friend_id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+  """,
+  """
+  DROP TABLE friends;
+  """
+)
