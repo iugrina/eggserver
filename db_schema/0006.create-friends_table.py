46c19d0fb1451309d9d9c9ad9e8a8f2c7fabb321
Ivo Ugrina <ivo@iugrina.com>
2012-10-13 14:10:12 +0200

    small change in create-friends_table

--- db_schema/0006.create-friends_table.py
+++ db_schema/0006.create-friends_table.py
@@ -3,6 +3,7 @@ step(
   CREATE TABLE `friends` (
     `user_id` bigint(20) unsigned NOT NULL,
     `friend_id` bigint(20) unsigned NOT NULL,
+    `approved` tinyint(1) unsigned NOT NULL,
     PRIMARY KEY (`user_id`,`friend_id`)
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
