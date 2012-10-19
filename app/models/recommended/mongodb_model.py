2e78b4424df76787b8a64971f17bec75025b0bc6
Ivo Ugrina <ivo@iugrina.com>
2012-10-19 11:48:54 +0200

    added basic recommended model/controller

---
+++ app/models/recommended/mongodb_model.py
@@ -0,0 +1,18 @@
+from mongokit import Document
+
+class Recommended(Document):
+    __database__ = "eggdb"
+    __collection__ = "recommended"
+    
+    structure = {
+        "user_id": int,
+        "recommended": [ {
+            "user_id": int,
+            "score": float,
+        } ]
+    }
+
+
+    required_fields = [ 'user_id' ]
+    use_schemaless = True
+
