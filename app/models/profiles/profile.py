587b2dda8d6459adf6cae2d5b55d841f0430d61c
bonidjukic <bonidjukic@gmail.com>
2013-04-13 01:02:38 +0200

    Bugfix: return error if wrong username entered & password field is not empty

--- app/models/profiles/profile.py
+++ app/models/profiles/profile.py
@@ -68,17 +68,18 @@ class Profile:
             user.username == params['username'])).execute()
         row = result.fetchone()
 
-        if bcrypt.hashpw(params['password'], row.password) == row.password:
-            data = dict(row)
-            data['created'] = str(data['created'])
-            data['dob'] = str(data['dob'])
-            data['password'] = None
-            data['first_name'] = utils.str2unicode(data['first_name'])
-            data['last_name'] = utils.str2unicode(data['last_name'])
-            return data
-        else:
-            return False
-        raise egg_errors.QueryNotPossible
+        if row is not None:
+            if bcrypt.hashpw(params['password'], row.password) == row.password:
+                data = dict(row)
+                data['created'] = str(data['created'])
+                data['dob'] = str(data['dob'])
+                data['password'] = None
+                data['first_name'] = utils.str2unicode(data['first_name'])
+                data['last_name'] = utils.str2unicode(data['last_name'])
+                return data
+            else:
+                return False
+            raise egg_errors.QueryNotPossible
 
     def signup(self, params):
         "Sign up the user"
